97879e9e1d9ba5c3ad31d92ab6805d259f3628fc
l1109901 <l1109901@gmail.com>
2016-03-04 21:32:53 +0200

    add Education.java

---
+++ app/src/main/java/ru/mail/loginregister/Education.java
@@ -0,0 +1,25 @@
+package ru.mail.loginregister;
+
+/**
+ * Created by gafur on 3/4/2016.
+ */
+public class Education {
+
+    private String okul_adi,bolum;
+    private int mezuniyet_yili;
+
+    public Education(String okul_adi,String bolum,int mezuniyet_yili){
+        this.okul_adi=okul_adi;
+        this.bolum=bolum;
+        this.mezuniyet_yili=mezuniyet_yili;
+    }
+
+    public void setOkul_adi(String okul_adi){this.okul_adi=okul_adi;}
+    public String getOkul_adi(){return okul_adi;}
+
+    public void setBolum(String bolum){this.bolum=bolum;}
+    public String getBolum(){return bolum;}
+
+    public void setMezuniyet_yili(int mezuniyet_yili){this.mezuniyet_yili=mezuniyet_yili;}
+    public int getMezuniyet_yili(){return mezuniyet_yili;}
+}
